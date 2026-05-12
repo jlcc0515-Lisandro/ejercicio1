14cd43cc0b0feeb2a243081f08823b1b0d4ef8a6
jlcc0515-Lisandro <jlcc0515@gmail.com>
2026-05-12 18:40:03 -0500

    feat: implementar bucle for para notas

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,10 @@
 nombre = input("nombre del estudiante: ")
 suma_notas = 0
+
+for i in range(1, 6):
+    nota = float(input(f"ingresa la nota {i}: "))
+    suma_notas += nota
+
+promedio = suma_notas / 5
+print(f"estudiante: {nombre}")
+print(f"promedio final: {promedio:.2f}")
