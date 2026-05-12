62aaf17c2c07469ff58bf8b1b4c8a82689697f72
jlcc0515-Lisandro <jlcc0515@gmail.com>
2026-05-12 18:48:03 -0500

    feat: implementar ciclo while

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,17 +1,25 @@
-nombre = input("nombre del estudiante: ")
-suma_notas = 0
+continuar = "si"
 
-for i in range(1, 6):
-    nota = float(input(f"ingresa la nota {i}: "))
-    suma_notas += nota
+while continuar == "si":
+    print("--- sistema de evaluación academica ---")
+    nombre = input("nombre del estudiante: ")
+    suma_notas = 0
 
-promedio = suma_notas / 5
-print(f"estudiante: {nombre}")
-print(f"promedio final: {promedio:.2f}")
+    for i in range(1, 6):
+        nota = float(input(f"ingresa la nota {i}: "))
+        suma_notas += nota
 
-if promedio >= 4.5:
-    print("estado: Excelente")
-elif promedio >= 3.0:
-    print("estado: aprobado")
-else:
-    print("estado: reprobado")
+    promedio = suma_notas / 5
+    print(f"estudiante: {nombre}")
+    print(f"promedio final: {promedio:.2f}")
+
+    if promedio >= 4.5:
+        print("estado: Excelente")
+    elif promedio >= 3.0:
+        print("estado: aprobado")
+    else:
+        print("estado: reprobado")
+
+    continuar = input("¿Deseas evaluar a otro estudiante? (si/no): ")
+
+print("gracias por usar el sistema ¡muchos exitos en tu evaluacion!")
